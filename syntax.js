12da2cec73dc4c90048502326ed7aecce4c727ae
Herlin Matos <herlin@everymundo.com>
2021-03-22 03:34:19 -0400

    fix backtracking issue
    now the character cursor is moved and passed through function arguments

--- syntax.js
+++ syntax.js
@@ -1,29 +1,20 @@
-var tokens = ['id','+','id', '+', 'id'];
+var tokens = ['id','+','id', '-', 'id'];
 
 //Grammar:
-//Exp => id + Exp | id 
+//Exp => id + Exp | id - Exp | id 
 
-var head = 0;
-
-var move = () => {
-	if(head < tokens.length-1)
-		head++;
-}
-
-var match = (t) => {
-	if(t == tokens[head]){
-		move();
+var match = (t, i) => {
+	if(t == tokens[i]){
 		return true;
 	}
 	return false;	
 }
 
-const Exp1 = () => { if(match('id') && match('+') && (e = Exp())) return {id:'id', o:'+', e} }
-const Exp2 = () => { if(match('id')) return {id:'id'} }
-const Exp = () => Exp1() || Exp2()
+const Exp1 = (i) => { if(match('id',i++) && match('+',i++) && (e = Exp(i))) return {id:'id', o:'+', e} }
+const Exp2 = (i) => { if(match('id',i++) && match('-',i++) && (e = Exp(i))) return {id:'id', o:'-', e} }
+const Exp3 = (i) => { if(match('id',i++)) return {id:'id'} }
+const Exp = (i) => Exp1(i) || Exp2(i) || Exp3(i)
 
 
-console.log('------\n',Exp());
+console.log('------\n',Exp(0));
 
-if(head < tokens.length-1)
-	console.log('error1');
